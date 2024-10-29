32511492bc4bb568c5f8c06ac0bda9dc1be90c48
Klins <www.klinsmann@gmail.com>
2024-10-29 23:05:46 +0000

    Navigation Library implemented for Mapbox

--- FrontEnd/TripCuts/src/screens/Map.tsx
+++ FrontEnd/TripCuts/src/screens/Map.tsx
@@ -1,40 +1,21 @@
-import React, {Component} from 'react';
-import {StyleSheet, View} from 'react-native';
-import Mapbox, {MapView} from '@rnmapbox/maps';
+import MapboxNavigation from '@pawan-pk/react-native-mapbox-navigation';
+import {StyleSheet} from 'react-native';
 
-Mapbox.setAccessToken(
-  'pk.eyJ1Ijoia2xpbnNtYW5uIiwiYSI6ImNtMjN3bGhmejBhanEycXFzY3FmcHoyem4ifQ.wTTs41xza38fGRIEsrdp7g',
-);
+export default function App() {
+  return (
+    <MapboxNavigation
+      startOrigin={{latitude: 53.3965812, longitude: -2.9804447}} // Your current location
+      destination={{latitude: 51.47002, longitude: -0.454295}} // London Heathrow Airport
+      style={styles.container}
+      shouldSimulateRoute={false}
+      showCancelButton={true}
+      language="en"
+    />
+  );
+}
 
 const styles = StyleSheet.create({
-  page: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
   container: {
-    height: '100%',
-    width: '100%',
-    backgroundColor: 'tomato',
-  },
-  map: {
     flex: 1,
   },
 });
-
-export default class Map extends Component {
-  componentDidMount() {
-    Mapbox.setTelemetryEnabled(false);
-  }
-
-  render() {
-    return (
-      <View style={styles.page}>
-        <View style={styles.container}>
-          <MapView style={styles.map} />
-        </View>
-      </View>
-    );
-  }
-}
